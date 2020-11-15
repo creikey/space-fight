154a50e669c5b5acae9dbcd88e882dce3daaa390
Cameron Reikes <cameronreikes@gmail.com>
2020-11-14 19:44:21 -0800

    Set dead body transform on spawn

--- src/Component_NetworkedRigidBody.gd
+++ src/Component_NetworkedRigidBody.gd
@@ -6,11 +6,11 @@ onready var to_control: RigidBody2D = get_parent()
 
 
 var last_frame_received: int = 0
-var _puppet_transform : Transform2D = Transform2D(0.0, Vector2()) # set from the master
+var puppet_transform : Transform2D = Transform2D(0.0, Vector2()) # set from the master
 
 puppet func receive_transform(new_transform: Transform2D, frame: int):
 	if frame > last_frame_received:
-		_puppet_transform = new_transform
+		puppet_transform = new_transform
 		last_frame_received = frame
 
 func _physics_process(delta):
@@ -18,4 +18,4 @@ func _physics_process(delta):
 		rpc_unreliable("receive_transform", to_control.global_transform, gamestate.frame)
 	else:
 #	printt(global_transform, global_transform, 0.2)
-		to_control.global_transform = to_control.global_transform.interpolate_with(_puppet_transform, 0.2)
+		to_control.global_transform = to_control.global_transform.interpolate_with(puppet_transform, 0.2)
--- src/DeadBody.gd
+++ src/DeadBody.gd
@@ -5,4 +5,5 @@ class_name DeadBody
 func create_from_state(state: Dictionary):
 	for p in state.keys():
 		set(p, state[p])
+	$Component_NetworkedRigidBody.puppet_transform = global_transform
 	$PlayerShape.disabled = not is_network_master()
